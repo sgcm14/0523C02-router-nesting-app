b9714aca42245d491cd5588ca01263190869c203
sgcm14 <sgcm14@gmail.com>
2024-09-08 12:08:23 -0500

    fix routing config and 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/0523C02-router-nesting-app/", // Ruta del repositorio
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "404.html", // Origen del archivo
+          dest: "", // Destino: la raíz de la carpeta `dist`
+        },
+      ],
+    }),
+  ],
 });
