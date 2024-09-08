c4dcb8b6521e25c2ed1ffa4b1534dd81c4948d92
sgcm14 <sgcm14@gmail.com>
2024-09-08 13:34:42 -0500

    fix deploy 2nd v

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/0523C02-router-nesting-app/", // Ruta del repositorio
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "404.html", // Origen del archivo
-          dest: "", // Destino: la raíz de la carpeta `dist`
-        },
-      ],
-    }),
-  ],
+  base: "https://sgcm14.github.io/0523C02-router-nesting-app/", // Ruta del repositorio
+  // base:"/",
+  plugins: [react()],
 });
